bc75031e4a298db4aa5c3847106eeb4f6cefbb5d
manesSaraj <manes.saraj@gmail.com>
2020-06-04 07:54:03 -0400

    Setting up spring boot application

---
+++ src/main/java/com/smanes/dataserver/DataServerApplication.java
@@ -0,0 +1,13 @@
+package com.smanes.dataserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataServerApplication.class, args);
+	}
+
+}
