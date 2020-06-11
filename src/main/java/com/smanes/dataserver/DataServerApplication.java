0632f850a125bcbd4ce639c0c6ea0f56d2f4ebd5
manesSaraj <manes.saraj@gmail.com>
2020-06-11 08:14:44 -0400

    Extracting an bean/Controller from ApplicationContext

--- src/main/java/com/smanes/dataserver/DataServerApplication.java
+++ src/main/java/com/smanes/dataserver/DataServerApplication.java
@@ -1,13 +1,18 @@
 package com.smanes.dataserver;
 
+import com.smanes.dataserver.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DataServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataServerApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DataServerApplication.class, args);
+		MyController controller  = (MyController) context.getBean(("myController"));
+		String myGreeting = controller.sayHello();
+		System.out.println(myGreeting);
 	}
 
 }
